6aaf2b7f74c8d8a4393ecafc1fa36a6f69c2cb90
Pavan Kumar <pavankmr1902@gmail.com>
2025-06-26 16:14:42 +0530

    removed redundant code

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,3 @@
-// const Button = ({ text, variant }) => {
-//   const base = 'px-5 py-2 rounded-md font-semibold shadow-lg';
-//   const styles = variant === 'primary'
-//     ? `${base} bg-orange-400 hover:bg-orange-500 text-black`
-//     : `${base} bg-transparent border border-white text-white hover:bg-white hover:text-black`;
-
-//   return <button className={styles}>{text}</button>;
-// };
-
-// export default Button;
-
 const Button = ({ text, variant = 'primary' }) => {
   const baseClasses = 'px-4 py-2 h-auto text-sm rounded font-semibold';
 
